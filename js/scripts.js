8c4c689ab2d23044fce3759d2beb2e4031f84daa
Vlad Melnikov <smoer404@gmail.com>
2020-06-22 15:17:49 +0300

    new edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,3 @@
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//     document.getElementById("navbar").style.padding = "0px 0px";
-//     document.getElementById("navbar").style.background = "white";
-//   } else {
-//     document.getElementById("navbar").style.padding = "5px 0px";
-//     document.getElementById("navbar").style.background = "white";
-//   }
-// }
-
-
 $('.news').slick({
   dots: false,
   infinite: true,
@@ -46,9 +32,6 @@ $('.news').slick({
         slidesToScroll: 1
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ]
 });
 
@@ -179,70 +162,69 @@ $('.calc').click(function(){
   $('.cost-result').fadeIn(500);
 
   if(selectedLevel == 1 && selectedForm == 1){
-
     selectedSpec = 174000;
     result = selectedSpec - selectedSpec / 100 * selectedYear;
     cost = selectedSpec / 100 * selectedYear;
-    
     summ = selectedSpec * selectedYearCount - selectedSpec / 100 * selectedYear;
+    summCost = cost * selectedYearCount;
     $('#summ').empty().text(summ);
     $('#result').empty().text(result);
     $('#cost').empty().text(cost);
-    
     $('#summCost').empty().text(summCost);
-
-
   } else if (selectedLevel == 2 && selectedForm == 1){
 
     selectedSpec = 186000;
-    result = selectedSpec - (selectedSpec / 100 * selectedYear);
+    result = selectedSpec - selectedSpec / 100 * selectedYear;
     cost = selectedSpec / 100 * selectedYear;
     summ = selectedSpec * selectedYearCount - selectedSpec / 100 * selectedYear;
+    summCost = cost * selectedYearCount;
     $('#summ').empty().text(summ);
     $('#result').empty().text(result);
     $('#cost').empty().text(cost);
-
+    $('#summCost').empty().text(summCost);
   } else if (selectedLevel == 1 && selectedForm == 2){
 
     selectedSpec = 60000;
-    console.log(selectedSpec);
-    result = selectedSpec - (selectedSpec / 100 * selectedYear);
+    result = selectedSpec - selectedSpec / 100 * selectedYear;
     cost = selectedSpec / 100 * selectedYear;
     summ = selectedSpec * selectedYearCount - selectedSpec / 100 * selectedYear;
+    summCost = cost * selectedYearCount;
     $('#summ').empty().text(summ);
     $('#result').empty().text(result);
     $('#cost').empty().text(cost);
-
+    $('#summCost').empty().text(summCost);
   } else if (selectedLevel == 2 && selectedForm == 2){
 
     selectedSpec = 80000;
-    console.log(selectedSpec);
-    result = selectedSpec - (selectedSpec / 100 * selectedYear);
+    result = selectedSpec - selectedSpec / 100 * selectedYear;
     cost = selectedSpec / 100 * selectedYear;
     summ = selectedSpec * selectedYearCount - selectedSpec / 100 * selectedYear;
+    summCost = cost * selectedYearCount;
     $('#summ').empty().text(summ);
     $('#result').empty().text(result);
     $('#cost').empty().text(cost);
+    $('#summCost').empty().text(summCost);
   } else if (selectedLevel == 3 && selectedForm == 1){
 
     selectedSpec = 205000;
-    console.log(selectedSpec);
-    result = selectedSpec - (selectedSpec / 100 * selectedYear);
+    result = selectedSpec - selectedSpec / 100 * selectedYear;
     cost = selectedSpec / 100 * selectedYear;
+    summ = selectedSpec * selectedYearCount - selectedSpec / 100 * selectedYear;
+    summCost = cost * selectedYearCount;
+    $('#summ').empty().text(summ);
     $('#result').empty().text(result);
     $('#cost').empty().text(cost);
-    summ = selectedSpec * selectedYearCount - selectedSpec / 100 * selectedYear;
-    $('#summ').empty().text(summ);
+    $('#summCost').empty().text(summCost);
   } else if (selectedLevel == 3 && selectedForm == 2){
-
     selectedSpec = 70000;
-    console.log(selectedSpec);
-    result = selectedSpec - (selectedSpec / 100 * selectedYear);
+    result = selectedSpec - selectedSpec / 100 * selectedYear;
     cost = selectedSpec / 100 * selectedYear;
+    summ = selectedSpec * selectedYearCount - selectedSpec / 100 * selectedYear;
+    summCost = cost * selectedYearCount;
+    $('#summ').empty().text(summ);
     $('#result').empty().text(result);
     $('#cost').empty().text(cost);
-    summ = selectedSpec * selectedYearCount - selectedSpec / 100 * selectedYear;
-    $('#summ').empty().text(summ);
+    $('#summCost').empty().text(summCost);
   } else {
     $('.cost-form select').siblings('.select-selected').addClass('warning');
     setTimeout(removeClass, 1000);
@@ -250,6 +232,18 @@ $('.calc').click(function(){
 });
 /* условия по калькулятору */
 
+$('.feedback-btn').click(function(){
+  $('body input').val("");
+  $('.feedback-overlay').fadeIn(500);
+});
+
+$('.close-btn').click(function(){
+  $(this).parent().parent().fadeOut(500);
+});
+
+$('.question-btn').click(function(){
+  $('.form-question__wrapper').toggle(500);
+})
 document.getElementById("defaultOpen").click();
 function openCity(evt, cityName) {
   // Declare all variables
@@ -271,3 +265,5 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+
